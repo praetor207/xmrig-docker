eeac45bad81c1ce85ae930add6ec75cd761605ec
praetor242 <gwesfisher@gmail.com>
2024-01-19 00:40:41 -0600

    changed wallets...again

--- mine.sh
+++ mine.sh
@@ -6,6 +6,6 @@ docker run -it \
     praetor242/xmrig:latest \
     --coin monero \
     -o stratum+tcp://xmr-us-east1.nanopool.org:10300 \
-    -u 44H4DPKj9PQ7XWLprDVBhzEbyEZMcw4XsZRkt9FatqY12SejeRKGj4Acquh3jhbFNZLGgJvZh5Qig3gfzqKDx4RKQJ7ooNU \
+    -u 49J59fZzo19PcK1v2kqXuuBBQZStnvxzJbkGEfxVai9uD72bhuDwjAfQ8Df134xKc4LGWGvEQ8TLoaiQ7gDNcgpyEPcS8Hh \
     -p M@ck3y242
 
