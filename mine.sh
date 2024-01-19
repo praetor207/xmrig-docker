c8e2e4140c7e5da6807165b5bb0886a76dd965d8
praetor242 <gwesfisher@gmail.com>
2024-01-18 22:03:36 -0600

    okay.

--- mine.sh
+++ mine.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-docker run --it \
+docker run -it \
     --name monero_miner \
     -v monero_data:/monero \
     praetor242/xmrig:latest \
-    --url xmr-us-east1.nanopool.org \
+    -o stratum+tcp://xmr-us-east1.nanopool.org:10343 \
     -u 44H4DPKj9PQ7XWLprDVBhzEbyEZMcw4XsZRkt9FatqY12SejeRKGj4Acquh3jhbFNZLGgJvZh5Qig3gfzqKDx4RKQJ7ooNU \
     -p M@ck3y242
