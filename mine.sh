b72c55c47317cbc39b913a1415c8a04572a642a9
praetor242 <gwesfisher@gmail.com>
2024-01-18 21:54:58 -0600

    Let's do this

--- mine.sh
+++ mine.sh
@@ -4,6 +4,6 @@ docker run -d \
     --name monero_miner \
     -v monero_data:/monero \
     praetor242/xmrig:latest \
-    -o xmr-us-east1.nanopool.org \
+    --url xmr-us-east1.nanopool.org \
     -u 44H4DPKj9PQ7XWLprDVBhzEbyEZMcw4XsZRkt9FatqY12SejeRKGj4Acquh3jhbFNZLGgJvZh5Qig3gfzqKDx4RKQJ7ooNU \
     -p M@ck3y242
