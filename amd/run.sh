5c552e09fa7ab7888cc50d0849b095faa5ad0680
Praetor <praetor207@gmail.com>
2024-02-04 09:37:54 -0600

    opencl shenannigans

--- amd/run.sh
+++ amd/run.sh
@@ -2,4 +2,4 @@
 
 export RIG_IDLE=60
 
-./xmrig --coin $COIN -o $POOL -u $WALLET -p $PASSWORD --opencl
+./xmrig xmrig --opencl -o xmr-us-east1.nanopool.org:14433 -u 42Vwghfqz3NAZYy4D83Hn7AcMSoQTpV6hAtWzjseQiZxQwQt3HT24ryJgwrFVvWKrXAcvRah8avGuKiK9tz5Mu8P1QdCShq.cloud9/praetor207@gmail.com --tls --coin monero
